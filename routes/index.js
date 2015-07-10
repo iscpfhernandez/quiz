4562f6a6d7711d9e6075b269a1f20720ee160906
pfhernandez <iscpfhernandez@gmail.com>
2015-07-10 16:22:03 -0500

    quiz author foto

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
