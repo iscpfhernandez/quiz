92888aa85b7e86493bcbc75c7d37db8eaee9327a
pfhernandez <iscpfhernandez@gmail.com>
2015-08-04 13:21:57 -0500

    Paso 18 Moderar comentarios

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,10 @@ module.exports = function(sequelize, DataTypes)
  return sequelize.define('Comment',
         {texto:{
            type: DataTypes.STRING,
-           validate: {notEmpty: {msg: "-> Falta comentario"}}}         
+           validate: {notEmpty: {msg: "-> Falta comentario"}}},
+           publicado :{
+            type: DataTypes.BOOLEAN,
+            defaultValue:false
+           }
         });
 }
--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 //definicion de rutas de session
 //formulario de login
@@ -40,5 +41,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Para agregar comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
